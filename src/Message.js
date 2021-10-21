b1c83df9b8c2f5f8af25be8aee8363523a8ed88f
Aaron Williams <aaronrw4@yahoo.com>
2021-10-21 02:03:58 -0700

    Working code

---
+++ src/Message.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Message extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {display: false};
+        this.changeDisplay = this.changeDisplay.bind(this);
+    }
+    
+    changeDisplay() {
+        this.setState({
+            display: !this.state.display
+        })
+    }
+    
+    render() {
+        const {display} = this.state;
+
+        let handleClick = () => {
+            this.changeDisplay()
+        }
+
+        return (
+            <React.Fragment>
+                <a onClick={handleClick}>
+                    Want to buy a new car?
+                </a>
+                {display ? 
+                    <p>Call +11 22 33 44 now!</p> :
+                    null
+                }
+            </React.Fragment>);
+    }
+}
+
+export default Message
