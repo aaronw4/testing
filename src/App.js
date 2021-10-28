c673fd185e03452453746c65345d893abf7cc937
Aaron Williams <aaronrw4@yahoo.com>
2021-10-28 13:52:27 -0700

    Removed other test code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import './App.css';
-import SubscribeButton from './Suscribe';
-import ShoppingList from './shoppingList';
-import AsyncParagraph from './AsyncParagraph';
-import List from './List';
 
-const sampleItems = [
-    {
-      name: 'TV',
-      id: 876234812
-    }
-  ];
 
 function App() {
   return (
     <div>
-      <SubscribeButton/>
-      <ShoppingList items={sampleItems}/>
-      <AsyncParagraph dataVersion="10"
-        loadData={ 
-                  () => {return new Promise((resolve, reject) => { resolve("Data!"); });} 
-        }
-      />
-      <List items={["A", "B", "C"]}/>
     </div>
   );
 }
